6f8b0ed8906ee65ceab3ad2793704e8b5a267e72
= <9189725+zhang-qingyou@user.noreply.gitee.com>
2024-01-20 11:22:37 +0800

    update docs content

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,38 +1,54 @@
 // .vuepress/config.js
 module.exports = {
+  head: [
+    ['link', { rel: 'icon', href: '/logo.png' }]
+  ],
   themeConfig: {
     // base: "",
     title: 'minivueBarrage',
+    description: '知识笔记',
     smoothScroll: true,
+   
     nav: [
       { text: '首页', link: 'https://xiaozhangclassmater.github.io/minivueBarrage-docs-web/#/dashboard', target: '_self', rel: '' },
       {
-        text: 'JavaScript笔记', link: '/web/javascript/javascript.md'
+        text: 'web知识广场',
+        items: [
+          {
+            text: 'JavaScript笔记', link: '/web/javascript/javascript.md'
+          },
+          {
+            text: 'vue笔记', link: '/web/vue/vue3.md'
+          },
+          {
+            text: 'react笔记', link: '/web/React/React.md'
+          },
+          {
+            text: 'webpack笔记', link: '/web/webpack/webpack.md'
+          },
+          {
+            text: '浏览器笔记', link: '/web/Browser/Browser.md'
+          },
+        ]
       },
       {
-        text: 'vue笔记', link: '/web/vue/vue3.md'
-      },
-      {
-        text: 'react笔记', link: '/web/React/React.md'
-      },
-      {
-        text: 'NodeJS笔记', link: '/web/node/Node.md'
-      },
-      {
-        text: 'webpack笔记', link: '/web/webpack/webpack.md'
+        text: 'NodeJs系列',
+        items: [
+          {
+            text: 'NodeJS笔记', link: '/web/node/Node.md'
+          },
+          {
+            text: 'Node_ORM框架笔记', link: '/web/ORM/ORM.md'
+          },
+          {
+            text: 'express笔记', link: '/web/express/express.md'
+          },
+        ]
       },
       {
         text: 'sql笔记', link: '/web/sql/mysql.md'
       },
-      {
-        text: '浏览器笔记', link: '/web/Browser/Browser.md'
-      },
-      {
-        text: 'Node_ORM框架笔记', link: '/web/ORM/ORM.md'
-      },
-      {
-        text: 'express笔记', link: '/web/express/express.md'
-      },
+      
       {
         text: '计算机网络笔记', link: '/web/netWork/netWork.md'
       },
@@ -40,7 +56,7 @@ module.exports = {
         text: '面试题', link: '/web/InterviewQuestion/InterviewQuestion.md'
       },
       {
-        text: '工作笔记', link: '/web/studyNote/index.md'
+        text: '工作笔记', link: '/web/studyNote/studyNote.md'
       }
     ],
     sidebar: [
@@ -117,69 +133,6 @@ module.exports = {
             path: '../web/studyNote/studyNote.md',
           },
         ]
-        // children: [
-        //   {
-        //     collapsable: true,
-        //     text: 'JavaScript',
-        //     path: '../web/javascript/javascript.md',
-        //   },
-        //   {
-        //     collapsable: true,
-        //     title: 'webpack',
-        //     path: '../web/webpack/webpack.md',
-        //   },
-        //   {
-        //     collapsable: true,
-        //     title: 'vue3',
-        //     path: '../web/vue/vue3.md',
-        //   },
-        //   {
-        //     collapsable: true,
-        //     title: 'React',
-        //     path: '../web/React/React.md',
-        //   },
-        //   {
-        //     collapsable: true,
-        //     title: 'NodeJS',
-        //     path: '../web/node/Node.md',
-        //   },
-        //   {
-        //     collapsable: true,
-        //     title: 'Mysql',
-        //     path: '../web/sql/mysql.md',
-        //   },
-        //   {
-        //     collapsable: true,
-        //     title: 'Node_ORM',
-        //     path: '../web/ORM/ORM.md',
-        //   },
-        //   {
-        //     collapsable: true,
-        //     title: 'express',
-        //     path: '../web/express/express.md',
-        //   },
-        //   {
-        //     collapsable: true,
-        //     title: 'Browser',
-        //     path: '../web/Browser/Browser.md',
-        //   },
-        //   {
-        //     collapsable: true,
-        //     title: '面试题',
-        //     path: '../web/InterviewQuestion/InterviewQuestion.md',
-        //   },
-        //   {
-        //     collapsable: true,
-        //     title: '计算机网络',
-        //     path: '../web/netWork/netWork.md',
-        //   },
-        //   {
-        //     collapsable: true,
-        //     title: '工作笔记',
-        //     path: '../web/studyNote/index.md',
-        //   },
-
-        // ],
         // initialOpenGroupIndex: -1 // 可选的, 默认值是 0
       }
     ]
--- docs/web/studyNote/studyNote.md
+++ docs/web/studyNote/studyNote.md
@@ -8,11 +8,22 @@
 
 如果匹配成功，`exec()` 方法返回一个数组，并更新正则表达式对象的 [`lastIndex`](https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/RegExp/lastIndex) 属性。完全匹配成功的文本将作为返回数组的第一项，从第二项起，后续每项都对应一个匹配的捕获组。数组还具有以下额外的[属性](https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/RegExp/exec)：
 
+
+
+将 正则 （）作为一个匹配组，第一项作为整个正则
+
+参数1：第一项结果为 完整的正则匹配结果
+
+参数2：将符合 第一组正则 `(\d+)` 匹配到的结果作为第二个参数
+
+>?: 忽略一组匹配的结果
+
+参数3：将第四组`(.\d+)` 正则匹配到的结果放入第三个参数
+
 **示例1**
 
 ~~~js
 /(\d+)(?:(.\d+)|)/.exec('100.2') // 输出 ['100.2' ，'123' , '.2' ， index： 0 ， groups: undefined , input:'123.2']
-
 ~~~
 
 ## 正则?:
@@ -35,4 +46,24 @@ str='abcde12abcfg'
 console.log(...str.matchAll(reg))
 // ['abcde', 'de'] ['abcfg', 'fg']
 ~~~
+
+## 正则的前瞻运算
+
+从 某个固定的位置 开始向前查找，不消耗字符，只要符合`?=` 的  就作为符合条件的匹配。
+
+~~~js
+/(\d{3})(?=\d)/g 
+~~~
+
+示例：
+
+~~~js
+1: 'abcd'.replace(/([a-z]{3})(?=\d)/g , '$1'+',')  // 'abcd'
+
+2: 'abc1'.replace(/([a-z]{3})(?=\d)/g , '$1'+',') // 'abc,1'
+
+3: '1231'.replace(/(\d{2})(?=(\d))/g , '$1' + '?') // '12?31'
+~~~
+
+
 
